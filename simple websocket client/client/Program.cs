c5a19bae2a9755d299eedcf5b8785373f620d9c9
phongsakorn <phongsakon09@gmail.com>
2016-05-04 11:20:36 +0700

    connect kurento media server with websocket ans send message json-rpc 2.0 to demo

---
+++ simple websocket client/client/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace client
+{
+    class Program
+    {
+        // class SenseWebSocketClient
+        private static SenseWebSocketClient client = null;
+
+        // path websocket 
+        static String url = "ws://192.168.182.135:8888/kurento";
+
+        // Message ตัวอย่างที่ส่งเข้าไป
+        static string[] cmd_meassage = { "{\"id\":1,\"method\":\"ping\",\"params\":{\"interval\":240000},\"jsonrpc\":\"2.0\"}",
+                                         "{\"id\":2,\"method\":\"create\",\"params\":{\"type\":\"MediaPipeline\",\"constructorParams\":{}},\"jsonrpc\":\"2.0\"}" };
+        
+        // สถานะของการ Connect ว่่าสามารถเชื่อมต่อกับ kurento media server โดยผ่าน websocket ได้หรือไม่
+        static Boolean connect_st = false;
+
+        // เริ่มการทำงาน websocket
+        static async Task Start_webSocket()
+        {
+            client = new SenseWebSocketClient(new Uri(url));
+            Console.WriteLine("Connecting to "+url);
+            connect_st = await client.Connect_webScoket();
+
+        }
+
+        // รับ message ที่ได้รับหลังจากที่ send message ไป
+        static async Task<string> GetRecieve()
+        {
+            var docList = await client.Receive();
+            Console.WriteLine("Receive : " + docList + "\n");
+            return docList;  
+        }
+
+        // Main การทำงาน
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============= Start =============");
+            Start_webSocket();      // เรื่มเชื่อมต่อ kms ผ่าน websocket
+
+            Thread.Sleep(1000);     // wait for a sec, so server starts and ready to accept connection..
+
+            int i = 0;
+
+            // เมื่อสามารถ connect kms ได้ผ่าน websocket
+            if(connect_st){
+                
+                // loop ในการส่ง message และรับ message 
+                while (i<cmd_meassage.Length)
+                {
+
+                    Console.WriteLine("*********** Message " + (i+1) + " *************\n");
+                    Console.WriteLine("===== Send message =====");
+                    
+                    // ส่ง message 
+                    client.SendCommand(cmd_meassage[i]);
+                    Console.WriteLine("Send    : " + cmd_meassage[i]+ "\n");
+
+                    Console.WriteLine("===== Receive message =====");
+                    // รับ message
+                    GetRecieve();
+
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                    Console.Clear();
+                    i++;
+                }
+            }
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ simple websocket client/client/SenseWebSocketClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;  
+
+namespace client
+{
+    public class SenseWebSocketClient
+    {
+        private ClientWebSocket _client;
+        public Uri _senseServerURI;
+
+        public SenseWebSocketClient(Uri senseServerURI)
+        {
+            _client = new ClientWebSocket();
+            _senseServerURI = senseServerURI;
+        }
+
+        // Connect websocket
+        public async Task<Boolean> Connect_webScoket()
+        {
+            Boolean result = false;
+
+            Thread.Sleep(1000); //wait for a sec, so server starts and ready to accept connection..
+
+            try
+            {
+                await _client.ConnectAsync(_senseServerURI, CancellationToken.None);
+                Console.WriteLine("connected...\n");
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Exception: {0}", ex);
+                Console.WriteLine("Cannot connect webSocket...\n");
+                result = false;
+            }
+
+            return result;
+        }
+
+        private async Task ConnectToSenseServer()
+        {
+            await _client.ConnectAsync(_senseServerURI, CancellationToken.None);
+        }
+
+        // Send message ในรูปแบบของ JSON
+        public async Task SendCommand(string jsonCmd)
+        {
+            ArraySegment<byte> outputBuffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(jsonCmd));
+            await _client.SendAsync(outputBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        // รับ message 
+        public async Task<string> Receive()
+        {
+            var receiveBufferSize = 1024;
+            byte[] buffer = new byte[receiveBufferSize];
+            var result = await _client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            var resultJson = (new UTF8Encoding()).GetString(buffer);
+            return resultJson;
+        }
+    }  
+}
